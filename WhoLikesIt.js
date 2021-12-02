736f05c0fddcf1b7fc384ffe5e26e632b86e0ed0
noorah15 <noorah.sulaiman.elq.js@tuwaiq.edu.sa>
2021-12-02 14:49:24 +0300

    Find The Parity Outlier

--- WhoLikesIt.js
+++ WhoLikesIt.js
@@ -7,5 +7,12 @@ function likes(names) {
   if (names.length === 3)
     return names[0] + ", " + names[1] + " and " + names[2] + " like this";
 
-  return names[0] + ", " + names[1] + " and 2 others like this";
+  return (
+    names[0] +
+    ", " +
+    names[1] +
+    " and " +
+    (names.length - 2) +
+    " others like this"
+  );
 }
